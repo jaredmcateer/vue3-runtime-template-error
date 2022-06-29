9bc961f634ae40ad730dda7cde13d2f17c6ab248
Jared McAteer <jared.mcateer@gmail.com>
2022-06-29 09:19:22 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import VRuntimeTemplate from 'vue3-runtime-template'
+const template = "<div</div>"
+</script>
+
+<template>
+<v-runtime-template :template="template"></v-runtime-template>
+</template>
+
